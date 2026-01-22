cc15270d5dbc79fea20b15bf09b0c4acab210d42
Ted Kesgar <t.kesgar@outlook.com>
2026-01-22 11:10:26 +0700

    Add tests for bun

--- src/test/bun/hash.test.ts
+++ src/test/bun/hash.test.ts
@@ -1,32 +1,12 @@
 import format from "@phc/format";
-import { afterAll, beforeAll, describe, it } from "vitest";
-import { hash, HashAlgorithm } from "../hash";
-import { phcIdToAlgorithm } from "../utils";
-import {
-  testHashArgon2,
-  testHashPBKDF2,
-  testHashScrypt,
-} from "./fixtures/vectors";
+import { afterAll, beforeAll, describe, expect, it } from "bun:test";
+import { hash, HashAlgorithm } from "../../hash";
+import { phcIdToAlgorithm } from "../../utils";
+import { testHashPBKDF2, testHashScrypt } from "../fixtures/vectors";
 
 describe("hash", () => {
-  describe("argon2", () => {
-    it.for(testHashArgon2)(
-      "should generate correct hash ($hash)",
-      async (tc, { expect }) => {
-        const testPhc = format.deserialize(tc.hash);
-        const phcString = await hash(tc.password, {
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          algorithm: testPhc.id as any,
-          // eslint-disable-next-line @typescript-eslint/no-explicit-any
-          ...(testPhc.params! as any),
-          salt: testPhc.salt!,
-        });
-
-        const phc = format.deserialize(phcString);
-        expect(phc).toEqual(testPhc);
-      },
-    );
-  });
+  // Bun.password.hash Argon2 does not support providing a custom salt, so we
+  // cannot test it
 
   describe("pbkdf2", () => {
     describe("generate correct hash", () => {
@@ -34,9 +14,9 @@ describe("hash", () => {
         process.env.ALLOW_SHA1_HASH = "OK";
       });
 
-      it.for(testHashPBKDF2)(
+      it.each(testHashPBKDF2)(
         "should generate correct hash ($hash)",
-        async (tc, { expect }) => {
+        async (tc) => {
           const testPhc = format.deserialize(tc.hash);
           const phcString = await hash(tc.password, {
             ...phcIdToAlgorithm(testPhc.id),
@@ -46,6 +26,7 @@ describe("hash", () => {
           });
 
           const phc = format.deserialize(phcString);
+
           expect(phc).toEqual(testPhc);
         },
       );
@@ -55,21 +36,21 @@ describe("hash", () => {
       });
     });
 
-    it("should not allow PBKDF2 with digest = sha1", async ({ expect }) => {
-      await expect(() => {
-        return hash("password", {
+    it("should not allow PBKDF2 with digest = sha1", async () => {
+      await expect(
+        hash("password", {
           algorithm: HashAlgorithm.PBKDF2,
           i: 1000,
           digest: "sha1",
-        });
-      }).rejects.toThrowError("PBKDF2 hash with SHA1 is not allowed");
+        }),
+      ).rejects.toThrowError("PBKDF2 hash with SHA1 is not allowed");
     });
   });
 
   describe("scrypt", () => {
-    it.for(testHashScrypt)(
+    it.each(testHashScrypt)(
       "should generate correct hash ($hash)",
-      async (tc, { expect }) => {
+      async (tc) => {
         const testPhc = format.deserialize(tc.hash);
         const phcString = await hash(tc.password, {
           algorithm: "scrypt",
